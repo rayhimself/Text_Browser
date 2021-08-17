0bbb44391e1e689068ae23bbbdeeb18f18377ac4
rayhimself <rayhimself04@gmail.com>
2021-08-18 01:39:36 +0300

    Inintial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def rsult():
+    head = request.args['head']
+    par = request.args['par']
+    error = request.args['error']
+    return render_template('result.html', head=head, par=par, error=error)
+
+if __name__ == '__main__':
+    app.run()
---
+++ server.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+def parsing(res):
+    soup = BeautifulSoup(res.text, 'html.parser')
+    head = soup.find("h1").text
+    par = soup.find_all("p")
+    par_new = ""
+    for item in par:
+        par_new = par_new + item.text
+    return head, par_new
+
+@app.route('/', methods=['POST', 'GET'])
+def get_data():
+    URL = request.form['search']
+    error = ''
+    head = ''
+    par_new = ''
+    try:
+        r = requests.get(URL)
+        head, par_new = parsing(r)
+    except requests.exceptions.MissingSchema:
+        r = requests.get('https://' + URL)
+        head, par_new = parsing(r)
+    except requests.exceptions.ConnectionError:
+        error = "Error: Host unreachable"
+    return redirect("http://127.0.0.1:5000/result?head={}&par={}&error={}".format(head, par_new, error))
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000)
