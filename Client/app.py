e646232078d7a503c0c84839ad428c438a1cc9c2
rayhimself <rayhimself04@gmail.com>
2021-08-20 01:52:07 +0300

    .

--- Client/app.py
+++ Client/app.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
